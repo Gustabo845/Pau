6b3dc13e00ec486b2ff9e87f44ad4b20e119a45d
Gustabo845 <agusti.oyarzun@inslapineda.cat>
2024-04-17 16:10:51 +0000

    prova

--- tasques.php
+++ tasques.php
@@ -1,113 +1,111 @@
-
-0bash: line 1: tasques.php
 <?php
-if (PHP_SAPI === 'cli') {
-	echo  "Estas en el CLI\n";
-} else {
-	die("Este script solo se puede ejecutar en la linea de comandos (CLI).\n");
+if (PHP_SAPI !== 'cli') {
+    die("Este script solo se puede ejecutar en la línea de comandos (CLI).\n");
 }
 
 $archiu_json = 'tareas.json';
 
-function cargarTascaJSON($archiu) {
-	if (file_exists($archiu)) {
-		$json_data = file_get_contents($archiu);
-		return json_decode($json_data, true);
-	}
-	return [];
+function cargarTareasJSON($archiu) {
+    if (file_exists($archiu)) {
+        $json_data = file_get_contents($archiu);
+        return json_decode($json_data, true);
+    }
+    return [];
 }
 
-
-function guardarTareasJSON($archiu, $tasca){
-	$json_data = json_encode($tasca, JSON_PRETTY_PRINT);
-	if (file_put_contents($archiu, $json_data) !== false) {
-		echo "Les tasques s'han guardat correctament en el archiu $archiu.\n";
-	} else {
-		echo "Error al guardar les tasques enl $archiu.\n";
-	}
+function guardarTareasJSON($archiu, $tareas){
+    $json_data = json_encode($tareas, JSON_PRETTY_PRINT);
+    if (file_put_contents($archiu, $json_data) !== false) {
+        echo "Las tareas se han guardado correctamente en el archivo $archiu.\n";
+    } else {
+        echo "Error al guardar las tareas en $archiu.\n";
+    }
 }
 
-$tasques = cargarTascaJSON($archiu);
+$tareas = cargarTareasJSON($archiu_json);
 
-
-$tasques = [];
 function generarID($cadena) {
-	return substr(sha1($cadena), 0, 5);
+    return substr(sha1($cadena), 0, 5);
 }
 
-function afegirTasca($descripcio) {
-	global $tasques;
-	$id = generarID($descripcio);
-	$dataAfegit = date('Y-m-d H:i:s');
-	$tasques[$id] = [ 'descripcio' => $descripcio, 'dataAfegit' => $dataAfegit, 'completada' => false];
-	return $id;
+function afegirTarea($descripcion) {
+    global $tareas;
+    $id = generarID($descripcion);
+    $dataAfegit = date('Y-m-d H:i:s');
+    $tareas[$id] = [ 'descripcion' => $descripcion, 'dataAfegit' => $dataAfegit, 'completada' => false];
+    guardarTareasJSON($archiu_json, $tareas);
+    return $id;
 }
 
-function llistarTasques() {
-	global $tasques;
-	echo "Tasques pendents:\n";
-	foreach ($tasques as $id => $tasca) {
-		echo "- $id: {$tasca['descripcio']} (Afegida el {$tasca['dataAfegit']}";
-		if ($tasca['completada']) {
-			echo "completada\n";
-		} else {
-			echo "\n";
-		}
-	}
+function llistarTareas() {
+    global $tareas;
+    echo "Tareas pendientes:\n";
+    foreach ($tareas as $id => $tarea) {
+        echo "- $id: {$tarea['descripcion']} (Añadida el {$tarea['dataAfegit']})";
+        if ($tarea['completada']) {
+            echo " completada\n";
+        } else {
+            echo "\n";
+        }
+    }
 }
-function eliminarTasca($id) {
-	global $tasques;
-	if (isset($tasques[$id])) {
-		unset($tasques[$id]);
-		echo "La tasca $id s'ha eliminat.\n";
-	} else {
-		echo "No s'ha trobat cap tasca amb l'dentifcador $id.\n";
-	}
+
+function eliminarTarea($id) {
+    global $tareas;
+    if (isset($tareas[$id])) {
+        unset($tareas[$id]);
+        guardarTareasJSON($archiu_json, $tareas);
+        echo "La tarea $id se ha eliminado.\n";
+    } else {
+        echo "No se ha encontrado ninguna tarea con el identificador $id.\n";
+    }
 }
 
 while (true) {
-	echo "Opcions:\n";
-	echo "A. Afegir tasca\n";
-	echo "B. llistar tasques\n";
-	echo "C. Marcar tasca com a completada\n";
-	echo "D. Eiminar tasca\n";
-	echo "F. Sortir\n";
-	$opcio = readline();
+    echo "Opciones:\n";
+    echo "A. Añadir tarea\n";
+    echo "B. Listar tareas\n";
+    echo "C. Marcar tarea como completada\n";
+    echo "D. Eliminar tarea\n";
+    echo "F. Salir\n";
+    $opcion = readline();
 
-	switch ($opcio) {
-		case 'A':
-		case 'a':
-			echo "Introdueix la descripció de la tasca: ";
-			$descripcio = readline();
-			$id = afegirTasca($descripcio);
-			echo "S'ha afegit una nova tasca amb l'identificador $id.\n";
-			$tasques[] = ['id' => $id, 'descripcio' => $descripcio, 'completada' => false];
-			echo "Tasca afegida amb ID: $id\n";
-			break;
+    switch ($opcion) {
+        case 'A':
+        case 'a':
+            echo "Introduce la descripción de la tarea: ";
+            $descripcion = readline();
+            $id = afegirTarea($descripcion);
+            echo "Se ha añadido una nueva tarea con el identificador $id.\n";
+            break;
 
-		case 'B':
-		case 'b':
-			llistarTasques();
-			break;
-		case 'C':
-		case 'c':
-			echo "Introdueix l'identificador de la tasca a marcar com a completada: ";
-			$id = readline();
-			eliminarTasca($id);
-			break;
-		case 'D':
-		case 'd':
-			echo "Introdueix l'identificador de la tasca a eliminar: ";
-			$id = readline();
-			eliminarTasca($id);
-			break;
-		case 'F':
-		case 'f':
-			echo "Sortint del programa.\n";
-			exit(0);
-		default:
-			echo "Opcio no valida. Si us plau, selecciona una opció del 1 al 5.\n";
-			break;
-	}
+        case 'B':
+        case 'b':
+            llistarTareas();
+            break;
+
+        case 'C':
+        case 'c':
+            echo "Introduce el identificador de la tarea a marcar como completada: ";
+            $id = readline();
+            eliminarTarea($id);
+            break;
+
+        case 'D':
+        case 'd':
+            echo "Introduce el identificador de la tarea a eliminar: ";
+            $id = readline();
+            eliminarTarea($id);
+            break;
+
+        case 'F':
+        case 'f':
+            echo "Saliendo del programa.\n";
+            exit(0);
+
+        default:
+            echo "Opción no válida. Por favor, selecciona una opción del 1 al 5.\n";
+            break;
+    }
 }
 ?>
